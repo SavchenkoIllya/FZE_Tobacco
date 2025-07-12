ba9d480e05d9ef40d84ab922a735f33a4b877069
Savchenko Illya <savchenko.illya.work@gmail.com>
2025-07-12 03:05:39 +0200

    deploy fixes 3

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
-import { getLocales } from "@/app/actions";
 import { CookieNames } from "@/app/lib";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+import { undefined } from "zod";
 
 export async function middleware(request: NextRequest) {
   const cookiesStore = await cookies();
@@ -20,7 +20,8 @@ export async function middleware(request: NextRequest) {
   const formattedPathname = pathname.split("/").filter(Boolean);
 
   if (!availableLocales) {
-    availableLocales = await getLocales();
+    // availableLocales = await getLocales();
+    availableLocales = undefined;
 
     if (!availableLocales) {
       return NextResponse.json({ error: "Server problems" }, { status: 500 });
