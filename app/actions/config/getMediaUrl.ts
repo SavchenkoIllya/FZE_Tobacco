6f291cadd52965b4357e3f3a67bfd1666b05fae9
Savchenko Illya <savchenko.illya.work@gmail.com>
2025-07-13 18:10:43 +0200

    Media url fix 3

--- app/actions/config/getMediaUrl.ts
+++ app/actions/config/getMediaUrl.ts
@@ -1,3 +1,3 @@
 export const getMediaUrl = (url: string) => {
-  return `${process.env.NEXT_PUBLIC_STRAPI_API_URL}:${process.env.NEXT_PUBLIC_STRAPI_PORT}${url}`;
+  return `${process.env.NEXT_PUBLIC_STRAPI_API_URL}${url}`;
 };
